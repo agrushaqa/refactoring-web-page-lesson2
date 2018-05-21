3ecbd79d96c58001580f8d3e6171bae3e07c27c3
Artem.Grusha <gawgav@gmail.com>
2018-05-21 03:12:49 +0300

    dz8 fix waiting results

--- 8/test_selenide_page_artlebedev/src/main/java/pages/ArtLebedevPage.java
+++ 8/test_selenide_page_artlebedev/src/main/java/pages/ArtLebedevPage.java
@@ -1,24 +1,32 @@
 package pages;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsContainer;
+import com.codeborne.selenide.WebDriverRunner;
+import org.openqa.selenium.WebElement;
 
 import java.util.List;
 import java.util.logging.Logger;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.url;
 
 public class ArtLebedevPage {
-    Logger log = Logger.getLogger(ArtLebedevPage.class.getName());
+    private Logger log = Logger.getLogger(ArtLebedevPage.class.getName());
+    private String currentUrl;
     //public ideaMatrix blockIdeas;
     //public Menu mainMenu;
 
     public ArtLebedevPage(){
         open("https://www.artlebedev.ru/");
+        currentUrl = WebDriverRunner.url();
     }
 
     //public class Menu extends ElementsContainer{
         public ideaMatrix Inventar(){
             $("a[href=\"/tools/\"]").click();
+            currentUrl = WebDriverRunner.url();
             return new ideaMatrix(); //blockIdeas;
         }
     //}
@@ -26,7 +34,21 @@ public class ArtLebedevPage {
     public class ideaMatrix /* extends ElementsContainer */ {
         //public ideaResults blockIdeaResults;
 
-        String currentIdea = "";
+        public Condition url(final String checkUrl){
+            //@TODO не используется и похоже не работает
+            return new Condition("url") {
+                @Override
+                public boolean apply(WebElement element) {
+                    return WebDriverRunner.url().equalsIgnoreCase(checkUrl);
+                }
+
+                @Override
+                public String actualValue(WebElement element) {
+                    return WebDriverRunner.url();
+                }
+            };
+        }
+
         public ideaMatrix(){
             $("a#item-matrix div.descr div.title").click();
         }
@@ -36,14 +58,16 @@ public class ArtLebedevPage {
             String editFieldForFindIdea = "input.input";
             $(editFieldForFindIdea).clear();
             $(editFieldForFindIdea).setValue(idea);
-            $(editFieldForFindIdea).pressEnter();
+            $(editFieldForFindIdea).pressEnter();//.shouldNot(url(currentUrl));
+            $("div.col-self-tablet-12").shouldHave(text(idea));
             return new ideaResults(); //blockIdeaResults;
         }
 
         public class ideaResults /* extends ElementsContainer */{
-            private List<String> listIdeaResults = $$("div.col-self-tablet-12").texts();
+            private List<String> listIdeaResults;
 
             public ideaResults get(){
+                listIdeaResults = $$("div.col-self-tablet-12").texts();
                 return this;
             }
             public int count(){
@@ -59,5 +83,6 @@ public class ArtLebedevPage {
         }
     }
 
-
 }
+
+
