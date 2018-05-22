80a66f61d1ba37ede22feb34578a8adddbe10e5b
Artem.Grusha <gawgav@gmail.com>
2018-05-22 14:13:31 +0300

    fix notes

--- 8/test_selenide_page_artlebedev/src/main/java/pages/ArtLebedevPage.java
+++ 8/test_selenide_page_artlebedev/src/main/java/pages/ArtLebedevPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsContainer;
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.WebElement;
 
@@ -14,27 +15,28 @@ import static com.codeborne.selenide.WebDriverRunner.url;
 
 public class ArtLebedevPage {
     private Logger log = Logger.getLogger(ArtLebedevPage.class.getName());
-    private String currentUrl;
+    //private String currentUrl;
     //public ideaMatrix blockIdeas;
     //public Menu mainMenu;
 
     public ArtLebedevPage(){
         open("https://www.artlebedev.ru/");
-        currentUrl = WebDriverRunner.url();
+        //currentUrl = WebDriverRunner.url();
     }
 
     //public class Menu extends ElementsContainer{
         public ideaMatrix Inventar(){
             $("a[href=\"/tools/\"]").click();
-            currentUrl = WebDriverRunner.url();
+            //currentUrl = WebDriverRunner.url();
             return new ideaMatrix(); //blockIdeas;
         }
     //}
 
     public class ideaMatrix /* extends ElementsContainer */ {
         //public ideaResults blockIdeaResults;
+        private SelenideElement resultList;
 
-        public Condition url(final String checkUrl){
+/*        public Condition url(final String checkUrl){
             //@TODO не используется и похоже не работает
             return new Condition("url") {
                 @Override
@@ -47,19 +49,20 @@ public class ArtLebedevPage {
                     return WebDriverRunner.url();
                 }
             };
-        }
+        }*/
 
         public ideaMatrix(){
-            $("a#item-matrix div.descr div.title").click();
+            $("a#item-matrix").click();
         }
 
         public ideaResults findIdea(String idea){
             log.info("Start find idea about :" + idea);
-            String editFieldForFindIdea = "input.input";
-            $(editFieldForFindIdea).clear();
-            $(editFieldForFindIdea).setValue(idea);
-            $(editFieldForFindIdea).pressEnter();//.shouldNot(url(currentUrl));
-            $("div.col-self-tablet-12").shouldHave(text(idea));
+            SelenideElement editFieldForFindIdea = $("input.input");
+            editFieldForFindIdea.clear();
+            editFieldForFindIdea.setValue(idea);
+            editFieldForFindIdea.pressEnter();//.shouldNot(url(currentUrl));
+            resultList = $("div.col-self-tablet-12");
+            resultList.shouldHave(text(idea));
             return new ideaResults(); //blockIdeaResults;
         }
 
--- 8/test_selenide_page_artlebedev/src/test/java/BaseTest.java
+++ 8/test_selenide_page_artlebedev/src/test/java/BaseTest.java
@@ -1,17 +1,13 @@
-import get_data.WebBrowser;
 import org.junit.Before;
 
 import java.util.logging.Logger;
 
 public class BaseTest {
     Logger log = Logger.getLogger(BaseTest.class.getName());
-    WebBrowser browser;
 
     @Before
     public void prepare()
     {
         log.info("Test started");
-        browser = new WebBrowser();
-        browser.setDriver();
     }
 }
